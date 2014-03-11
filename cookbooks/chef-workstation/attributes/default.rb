412898a58c12989e238202f35c94b40d7931e10a
Hongbin Lu <hongbin034@gmail.com>
2014-03-11 17:58:13 +0000

    Removed cookbooks that are not needed anymore

--- cookbooks/chef-workstation/attributes/default.rb
+++
@@ -1,8 +0,0 @@
-default["chef_workstation"]["log_level"]              = :info
-default["chef_workstation"]["log_location"]           = "STDOUT"
-default["chef_workstation"]["conf_dir"]               = "/home/ubuntu/.chef"
-default["chef_workstation"]["api_client_name"]        = "client"
-default["chef_workstation"]["api_client_key_path"]    = "/home/ubuntu/.chef/client.pem"
-default["chef_workstation"]["chef_server_url"]        = "http://localhost:4000"
-default["chef_workstation"]["validation_client_name"] = "chef-validator"
-default["chef_workstation"]["validation_key_path"]    = "/home/ubuntu/.chef/validation.pem"
